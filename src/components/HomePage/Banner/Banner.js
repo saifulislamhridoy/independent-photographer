3a43120ab92f23b6458f0b544e1f9c733bd87630
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-04-17 23:28:23 +0600

    added banner

--- src/components/HomePage/Banner/Banner.js
+++ src/components/HomePage/Banner/Banner.js
@@ -3,8 +3,19 @@ import banner from '../../../banner/banner.mp4'
 
 const Banner = () => {
     return (
-        <div>
-            <video src={banner} type='video/mp4' autoPlay></video>
+        <div className='mt-20 z-[-1]'>
+           <video className=' object-cover z-[-1] w-full h-[90vh]' autoPlay loop muted>
+                <source src={banner} type="video/mp4"></source>
+                
+            </video>
+            <div className='absolute top-[300px] left-0 flex justify-center mx-2'>
+               <div className=' border-2 p-5 md:w-1/2'>
+               <h2 className='bg-pink-600 inline rounded text-2xl  px-2 py-2 text-white font-bold w-full'>Make Your Moment</h2>
+                <p className='text-white mt-2 text-1xl md:text-4xl'>We're Photoshot a small and enthusiastic photography studio based in Dhaka</p>
+                <button className='bg-white px-3 py-2 mt-2 text-xl rounded hover:bg-pink-600 hover:text-white duration-500 ease-in'>Book Now</button>
+               </div>
+            </div>
+           
         </div>
         
     );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     ]
     const [open, setOpen] =useState(false)
     return (
-        <div className='shadow-md w-full fixed top-0 left-0'>
+        <div className='shadow-md w-full fixed top-0 left-0 z-50'>
             <div className='md:flex items-center justify-between py-4 bg-white md:px-10 px-7'>
                 <div className='font-bold text-2xl cursor-pointer flex items-center font-[poppins] text-gray-800'>
                     <span className='text-3xl text-pink-500 mr-1 pt-2'>
